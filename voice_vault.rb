2b398c1ee84d85c730948f796d627c0710361407
Phil Hofmann <phil@200ok.ch>
2023-04-20 11:37:07 +0200

    minimal guix file, rename folder to path and support ~/ in config

--- voice_vault.rb
+++ voice_vault.rb
@@ -6,6 +6,9 @@ require 'date'
 require 'yaml'
 
 @config = YAML.load_file("#{ENV['HOME']}/.config/voice_vault/config.yml")
+%w(archive_path whisper_path).each do |path|
+  @config[path].gsub!(/^~\//, ENV['HOME'] + '/')
+end
 
 def get_sources(sources)
   input_source = ''
@@ -57,7 +60,7 @@ def encode_to_mp3(wav_file)
 end
 
 def save_files(folder, mp3_file, wav_file, transcribed_text)
-  `mkdir #{folder}`
+  `mkdir -p #{folder}`
   `mv #{mp3_file} #{folder}/recording.mp3`
   `rm #{wav_file}`
   File.write("#{folder}/transcription.txt", transcribed_text)
@@ -69,6 +72,6 @@ wav_file = `mktemp --dry-run --suffix=.wav`.strip
 capture_audio(wav_file, input_source, monitor_source)
 transcribed_text = transcribe_to_text(wav_file)
 mp3_file = encode_to_mp3(wav_file)
-folder = "#{@config['archive_folder']}/#{DateTime.now.strftime('%Y-%m-%d_%H-%M-%S')}"
+folder = "#{@config['archive_path']}/#{DateTime.now.strftime('%Y-%m-%d_%H-%M-%S')}"
 save_files(folder, mp3_file, wav_file, transcribed_text)
 puts "Recording and transcription: #{folder}"
